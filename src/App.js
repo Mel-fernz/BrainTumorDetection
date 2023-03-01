4368ed359b8de5fc474042ca2c9ace42174b3603
Melanie <melanie.fernz10@gmail.com>
2023-03-01 07:09:05 +0530

    sem 7 final changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import Navbar from './components/Navbar';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Patients from './pages/Patients';
 import Test from './pages/Test';
-import Logout from './pages/Logout';
 import Signup from './pages/Signup';
+import Logout from './pages/Logout';
+import Login from './pages/Login';
 
 function App() {
+const [showLogin, setShowLogin]=useState(false);
+
+  useEffect(() => {      
+    if (showLogin===false)
+    setShowLogin(true);
+    else
+    setShowLogin(false);
+  }, [])
+  
   return ( 
     <>
+    {/* {showLogin===true?<Login/>: */}
       <Router>
-        <Navbar />
+   
         <Switch>
-        <Route path='/' exact component={Signup} />
+          <Route path='/' exact component={Login} />
+          
+          <Route path='/signup' component={Signup} />
           <Route path='/home' component={Home} />
           <Route path='/patients' component={Patients} />
           <Route path='/test' component={Test} />
           <Route path='/logout' component={Logout} />
         </Switch>
-      </Router> 
+      </Router>
+       {/* } */}
     </>
   );
 }
